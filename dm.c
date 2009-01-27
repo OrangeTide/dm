d23e36a854d7e1ad4aaba218df4918886c793e27
Jon Mayo <jon.mayo@gmail.com>
2009-01-26 18:43:03 -0800

    game tick is called when server has clients. moved commands into a struct. exit names have aliases. look command can view another player's inventory. more macros to make the code smaller. number connections starting at 1 instead of 0. command-line argument of port number may be supplied.

--- dm.c
+++ dm.c
@@ -4,6 +4,7 @@
  */
 #include <arpa/inet.h>
 #include <ctype.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <signal.h>
 #include <stdarg.h>
@@ -14,13 +15,14 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#define V "0.5.1"
+#define VER "0.5.2"
 #define S(x) sizeof(x)
 #define N(x) (S(x)/S(*x))
 #define CK(s) if((s)<0) { perror(#s); exit(1); }
 #define F(x) for(a=0;a<N(c);a++) { x; }
 #define CM(f) void f(int b, char *s)
 #define CH(f) void f(int b)
+#define CD(f) void f(int b, int d)
 #define R "\r\n"
 #define KS ss(b, B.s);
 #define BI(x,d) (((x)>>d)&1)
@@ -29,6 +31,8 @@
 #define Rb r[c[b].r]
 #define RET return
 #define Iname(i) (id[(i).v].n)
+#define Z(p) memset(p, 0, sizeof *p) /* zero */
+sig_atomic_t tk_fl=1; /* tick timeout flag */
 enum {
 	rhand, lhand, head, body,
 };
@@ -36,8 +40,12 @@ struct ii { /* item instance */
 	int v; /* vnum */
 	int a;
 };
-const char *dn[]={"north", "south", "west", "east"};
-const char *q[]={"quit", "look", "n", "north", "s", "south", "w", "west", "e", "east", "say", "help", "name", "open", "o", "close", "c", "who", "exits", "take", "drop", "inventory", "i" };
+const char *dn[][3]={ /* direction name table and aliases */
+	{"north", "n"},
+	{"south", "s"},
+	{"west", "w"},
+	{"east", "e"}
+};
 struct {
 	int f, r, s; /* file descriptor, current room, state */
 	char n[16], b[999]; /* name and buffer */
@@ -78,19 +86,20 @@ void pr(int b, const char *s, ...) {
 	wr(b, u);
 }
 void sta(int b, int r, const char *s, ...) {
-	int a, l=strlen(B.n);
+	int a, l=b>=0?strlen(B.n):0;
 	char u[99];
 	va_list ap;
 	va_start(ap, s);
-	strcpy(u, B.n);
+	if(b>=0) strcpy(u, B.n);
 	vsnprintf(u+l, S(u)-l, s, ap);
 	F(if(a!=b&&A.f&&(r==-1||A.r==r)) { wr(a, u); });
 }
 int ldn(const char *s) {
-	int i;
+	int i, j;
 	for(i=0;i<N(dn);i++)
-		if(!strcasecmp(s, dn[i])) RET i;
-	RET 0;
+		for(j=0;dn[i][j];j++)
+			if(!strcasecmp(s, dn[i][j])) RET i;
+	RET -1;
 }
 char *iname(struct ii *ii, int o) {
 	static char b[99];
@@ -132,6 +141,33 @@ int idr(int b, struct ii *s) { /* drop item s to room that b is standing in */
 	imv(Rb.i+d, s);
 	RET 1;
 }
+void inv(int a, int b) {
+	char n[99]="You are ";
+	int o, f;
+	if(a!=b) {
+		snprintf(n, S(n), "%s is ", B.n);
+	}
+	pr(a, "%sweilding %s.\n", n, iname(B.i, rhand));
+	pr(a, "%sholding %s.\n", n, iname(B.i, lhand));
+	pr(a, "%swearing %s on your head.\n", n, iname(B.i, head));
+	pr(a, "%swearing %s on your body.\n", n, iname(B.i, body));
+	if(a!=b) {
+		pr(a, "%s's pack contains", B.n);
+	} else {
+		pr(a, "Your pack contains");
+	}
+	for(f=0,o=body+1;o<N(B.i);o++) {
+		if(B.i[o].v) {
+			pr(a, " %s", iname(B.i, o));
+			f++;
+		}
+	}
+	if(f) {
+		wr(a, "."R);
+	} else {
+		pr(a, " %s."R, id->n);
+	}
+}
 CH(cl) {
 	int o, d;
 	close(B.f);
@@ -150,16 +186,16 @@ CH(cl) {
 		}
 	}
 }
-void ss(int b, int s) {
-	B.s=s;
-	if(s==0) {
+CD(ss) {
+	B.s=d;
+	if(d==0) {
 		wr(b, ">");
 	}
 }
 CM(c_q) {
 	cl(b);
 }
-CM(c_l) {
+CM(c_lr) {
 	int a, f, i;
 	wr(b, Rb.d);
 	/* check for items on the floor */
@@ -182,9 +218,18 @@ CM(c_l) {
 	} else if(f) {
 		wr(b, "are here."R);
 	}
+}
+CM(c_l) {
+	if(*s) {
+		int a;
+		F(if(A.f&&A.r==B.r&&!strcasecmp(s, A.n)) { inv(b, a); KS; RET; });
+		wr(b, "I don't see that here."R);
+	} else {
+		c_lr(b, s);
+	}
 	KS;
 }
-void go(int b, int d) {
+CD(go) {
 	int t=Rb.p[d];
 	if(t<0) {
 		wr(b,"You can't go that way!"R);
@@ -200,18 +245,6 @@ void go(int b, int d) {
 	KS;
 	RET;
 }
-CM(c_n) {
-	go(b, 0); /* north */
-}
-CM(c_s) {
-	go(b, 1); /* south */
-}
-CM(c_w) {
-	go(b, 2); /* west */
-}
-CM(c_e) {
-	go(b, 3); /* east */
-}
 CM(c_say) {
 	int a;
 	scrub(s);
@@ -219,15 +252,7 @@ CM(c_say) {
 	F(if(a!=b&&A.f&&A.r==B.r) { pr(a, "%s says \"%s\""R, B.n, s); });
 	KS;
 }
-CM(c_h) {
-	int j;
-	for(j=0;j<N(q);j++) {
-		wr(b, q[j]);
-		wr(b, " ");
-	}
-	wr(b, R);
-	KS;
-}
+CM(c_h);
 CM(c_a) {
 	int a;
 	char *p;
@@ -243,8 +268,9 @@ CM(c_a) {
 	KS;
 }
 CM(c_o) {
-	int d=ldn(s), t=Rb.p[d];
-	if(BI(Rb.e&Rb.s, d)) {
+	int d=ldn(s);
+	if(d>=0&&BI(Rb.e&Rb.s, d)) {
+		int t=Rb.p[d];
 		Rb.s^=1<<d;
 		r[t].s^=1<<(d^1);
 		wr(b, "You open the exit."R);
@@ -256,8 +282,9 @@ CM(c_o) {
 	KS;
 }
 CM(c_c) {
-	int d=ldn(s), t=Rb.p[d];
-	if(BI(Rb.e, d)&&!BI(Rb.s, d)) {
+	int d=ldn(s);
+	if(d>=0&&BI(Rb.e, d)&&!BI(Rb.s, d)) {
+		int t=Rb.p[d];
 		Rb.s^=1<<d;
 		r[t].s^=1<<(d^1);
 		wr(b, "You close the exit."R);
@@ -270,7 +297,7 @@ CM(c_c) {
 }
 void wh(int b) {
 	int a, n=0;
-	F(if(A.f) { n++; pr(b, "%3u %s"R, a, A.n); });
+	F(if(A.f) { n++; pr(b, "%3u %s"R, a+1, A.n); });
 	pr(b, "%u users online."R"***"R, n);
 }
 CM(c_wh) {
@@ -281,7 +308,7 @@ CM(c_ex) {
 	int d, e;
 	for(d=0;d<N(Rb.p);d++) {
 		if((e=Rb.p[d])>=0) {
-			wr(b, dn[d]);
+			wr(b, dn[d][0]);
 			if(BI(Rb.e, d)) wr(b, BI(Rb.s, d)?"(opened)":"(closed)");
 			wr(b, " ");
 		}
@@ -328,35 +355,53 @@ CM(c_dr) { /* "drop" command */
 	KS;
 }
 CM(c_i) {
-	int o, f;
-	pr(b, "You are weilding %s.\n", iname(B.i, rhand));
-	pr(b, "You are holding %s.\n", iname(B.i, lhand));
-	pr(b, "You are wearing %s on your head.\n", iname(B.i, head));
-	pr(b, "You are wearing %s on your body.\n", iname(B.i, body));
-	pr(b, "Your pack contains");
-	for(f=0,o=body+1;o<N(B.i);o++) {
-		if(B.i[o].v) {
-			pr(b, " %s", iname(B.i, o));
-			f++;
-		}
-	}
-	if(f) {
-		wr(b, "."R);
-	} else {
-		pr(b, " %s."R, id->n);
-	}
+	inv(b, b);
 	KS;
 }
-CM((*f[N(q)]))={c_q, c_l, c_n, c_n, c_s, c_s, c_w, c_w, c_e, c_e, c_say, c_h, c_a, c_o, c_o, c_c, c_c, c_wh, c_ex, c_t, c_dr, c_i, c_i};
+const struct {
+	char *n[4];
+	CM((*f));
+} ct[] = { /* command table */
+	{{"quit"}, c_q},
+	{{"look", "l"}, c_l},
+	{{"say"}, c_say},
+	{{"help", "?"}, c_h},
+	{{"name"}, c_a},
+	{{"open", "o"}, c_o},
+	{{"close", "c"}, c_c},
+	{{"who"}, c_wh},
+	{{"exits", "ex"}, c_ex},
+	{{"take", "t"}, c_t},
+	{{"drop", "dr"}, c_dr},
+	{{"inventory", "i"}, c_i},
+};
+CM(c_h) {
+	int i, j;
+	for(i=0;i<N(ct);i++) {
+		for(j=0;ct[i].n[j];j++) {
+			wr(b, ct[i].n[j]);
+			wr(b, " ");
+		}
+	}
+	wr(b, R);
+	KS;
+}
 CH(sl) {
-	int j, l;
+	int i, j, l;
 	if(B.b[0]) {
-		for(j=0;j<N(q);j++) {
-			l=strcspn(B.b," ");
-			if(l==strlen(q[j]) && !strncmp(B.b, q[j], l)) {
-				while(isspace(B.b[l])) l++;
-				f[j](b, B.b+l);
-				goto x;
+		if((i=ldn(B.b))>=0) {
+			go(b, i);
+			goto x;
+		}
+		l=strcspn(B.b," ");
+		if(B.b[l]) B.b[l++]=0;
+		while(isspace(B.b[l])) l++;
+		for(i=0;i<N(ct);i++) {
+			for(j=0;ct[i].n[j];j++) {
+				if(!strcmp(B.b, ct[i].n[j])) {
+					ct[i].f(b, B.b+l);
+					goto x;
+				}
 			}
 		}
 		wr(b, "Try 'help' for commands."R);
@@ -365,26 +410,29 @@ CH(sl) {
 x:
 	B.b[0]=0;
 }
-CH(nc) {
-	B.b[0]=0;
-	B.r=0;
-	B.s=0;
-	memset(B.i, 0, S(B.i));
+CD(nc) {
+	Z(&B);
+	B.f=d;
 	snprintf(B.n, S(B.n), "Someone%u", b+1);
-	wr(b, "Welcome to Deathmatch!"R"*** version "V" ***"R"Type 'help' for help."R);
+	wr(b, "Welcome to Deathmatch!"R"*** version "VER" ***"R"Type 'help' for help."R);
 	wh(b);
 	c_l(b, "");
 }
-int main(void) {
+void sh_tk(int s) { tk_fl=1; } /* signal handler for ticks */
+int main(int ac, char **av) {
 	int s, a, n, m, t;
 	struct sockaddr_in x;
 	fd_set z;
 	socklen_t l;
 	char h[2]={0};
-	signal(SIGPIPE, SIG_IGN);
+	struct sigaction sa;
+	Z(&sa);
+	sa.sa_handler=sh_tk;
+	sa.sa_flags=SA_RESTART;
+	CK(sigaction(SIGALRM, &sa, 0));
 	CK(s=socket(PF_INET, SOCK_STREAM, 0));
-	memset(&x, 0, S(x));
-	x.sin_port=htons(5555);
+	Z(&x);
+	x.sin_port=htons(ac>1?atoi(av[1]):5555);
 	CK(bind(s, &x, S(x)));
 	CK(listen(s,1));
 	for(;;) {
@@ -392,11 +440,27 @@ int main(void) {
 		FD_SET(s, &z);
 		m=s;
 		F(if(A.f) { FD_SET(A.f, &z); if(A.f>m) m=A.f; });
-		CK(n=select(m+1, &z, 0, 0, 0));
+		if(m==s) {
+			puts("Zzz"); /* sleeping - no clients */
+			tk_fl=-1; /* disable ticks, go to sleep */
+			alarm(0);
+		} else if(tk_fl<0) {
+			tk_fl=1; /* force a tick */
+		}
+		if(tk_fl>0) { /* process tick */
+			/* sta(-1, -1, R"[tick]"R">"); */
+			tk_fl=0;
+			alarm(5); /* this is the tick rate */
+		}
+		n=select(m+1, &z, 0, 0, 0);
+		if(n<0) {
+			if(errno==EINTR) continue;
+			CK(-1);
+		}
 		if(FD_ISSET(s, &z)) {
 			l=S(x);
 			CK(t=accept(s, &x, &l));
-			F(if(!A.f) { A.f=t; nc(a); break; });
+			F(if(!A.f) { nc(a, t); break; }); /* find next free slot */
 			if(a==N(c)) {
 				close(t);
 			}
